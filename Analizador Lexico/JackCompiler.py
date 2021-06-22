945be92e867a5737cb92c887472d1a8a53c9fc73
otaviobelfort <otaviodeel@gmail.com>
2021-06-21 23:12:01 -0300

    class

---
+++ Analizador Lexico/JackCompiler.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+- Design of a lexical analyzer
+- Author: https://github.com/otaviocesar
+- Author: https://github.com/otaviobelfort
+
+'''
+
+        
---
+++ Analizador Lexico/JackTokenizer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+- Design of a lexical analyzer
+- Author: https://github.com/otaviocesar
+- Author: https://github.com/otaviobelfort
+
+'''
+
+class JackTocknizer:
+
+    def __init__(self):
+
+        return
