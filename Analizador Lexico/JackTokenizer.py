bbb550d55d37453673c253cc8557c7a43cb661fa
otaviobelfort <otaviodeel@gmail.com>
2021-06-22 18:04:55 -0300

    jack Tocknizer

--- Analizador Lexico/JackTokenizer.py
+++ Analizador Lexico/JackTokenizer.py
@@ -9,5 +9,33 @@
 class JackTocknizer:
 
     def __init__(self):
+        #  arquivo(.jack) com o script a ser compitalado
+        self.file_script_input = "script.jack"
 
-        return
+    def isLetter (self, string):
+        letter = "[a-z][A-Z]" # obs: definir regex ainda
+        if string in letter:
+            return True
+        return False
+
+     # Identifica se a entrada é um digito
+    def isDigit (self, string):
+        digit = '0123456789'
+        if string in digit:
+            return True
+        return False
+
+     # Identifica se a entrada é um operador
+    def isOperator(self, byte_in):
+        operators = '. + - * / ++ -- == != > >= < <= && || ='.split()
+        if byte_in in operators:
+            return True
+        return False
+
+    # Identifica se a entrada é um simbolo
+    def isSymbol(self, string):
+        symbols = ''' !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHJKLMNOPQRSTUVXWYZ[\]^_`abcdefghijklmnopqrstuvxwyz{|}~'''
+        if(string in symbols):
+            return True
+        return False
+
--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 	
 
 ## Analisador Léxico
+### Especificação da microsintaxe
 -------------------
 * Palavras reservadas: 
 
