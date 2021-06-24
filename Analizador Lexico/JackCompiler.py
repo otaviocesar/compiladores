34d8a4f2338646539138d00ced7fc9a783ffbe14
Otavio Cesar <o.caesar@hotmail.com>
2021-06-23 22:25:12 -0300

    Adicionando import JackTockenizer no JackCompiler

--- Analizador Lexico/JackCompiler.py
+++ Analizador Lexico/JackCompiler.py
@@ -5,5 +5,19 @@
 - Author: https://github.com/otaviobelfort
 
 '''
+from JackTockenizer import JackTockenizer
 
+def main():
+    # Chamando o arquivo main.jack e imprimindo os tokens enumerados.  
+    with open('main.jack', 'r') as f:
+        TEST_LINES = f.readlines()
+    TOKENIZER = JackTockenizer(TEST_LINES)
+    for i, token in enumerate(TOKENIZER.tokens):
+        print(i, token)
+        print(JackTockenizer.token_type(token))
+    print('-----------------')
+
+
+if __name__ == '__main__':
+    main()
         
