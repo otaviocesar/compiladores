7f193216a04b3a37457f9a11feaf935ce91acc18
otaviobelfort <otaviodeel@gmail.com>
2021-06-23 23:49:35 -0300

    add pycache

--- Analizador Lexico/JackCompiler.py
+++ Analizador Lexico/JackCompiler.py
@@ -5,16 +5,17 @@
 - Author: https://github.com/otaviobelfort
 
 '''
-from JackTockenizer import JackTockenizer
+import JackTokenizer
 
 def main():
     # Chamando o arquivo main.jack e imprimindo os tokens enumerados.  
     with open('main.jack', 'r') as f:
         TEST_LINES = f.readlines()
-    TOKENIZER = JackTockenizer(TEST_LINES)
+    TOKENIZER = JackTokenizer.JackTockenizer(TEST_LINES)
     for i, token in enumerate(TOKENIZER.tokens):
         print(i, token)
-        print(JackTockenizer.token_type(token))
+        print(JackTokenizer.JackTockenizer.token_type(token))
+        
     print('-----------------')
 
 
