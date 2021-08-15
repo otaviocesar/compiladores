fa1fb40cc7e06fd31405b407fd22f0f90b4c0cd1
otaviobelfort <otaviodeel@gmail.com>
2021-08-15 15:39:09 -0300

    up

--- codewrite/codeWrite.py
+++ codewrite/codeWrite.py
@@ -17,42 +17,47 @@ class Codewrite:
             "FIELD"   : "this"
         }
         # return ...
-        def push(self, kind, index):
-            receive_kind = self.kindDict.get(kind)
+    
+    def push(self, kind, index):
+        receive_kind = self.kindDict.get(kind)
 
-            if(receive_kind != None):
-                print("push {} {}".format(receive_kind,index),file=self.outfile)
+        if(receive_kind != None):
+            self._output("push {} {}".format(receive_kind,index),file=self.outfile)
             
-            else:
-                print("ERROR push")
+        else:
+            self._output("ERROR push")
         
-        def pop(self, kind, index):
-            receive_kind = self.kindDict.get(kind)
+    def pop(self, kind, index):
+        receive_kind = self.kindDict.get(kind)
 
-            if(receive_kind != None):
-                print("pop {} {}".format(receive_kind,index),file=self.outfile)
+        if(receive_kind != None):
+            self._output("pop {} {}".format(receive_kind,index),file=self.outfile)
             
-            else:
-                print("ERROR pop")
+        else:
+            self._output("ERROR pop")
 
-        def ifgotoWrite(self, label):
-            print("if-goto {}".format(label),file=self.outfile)
+    def ifgotoWrite(self, label):
+        self._output("if-goto {}".format(label),file=self.outfile)
 
-        def gotoWrite(self, label):
-            print("goto {}".format(label),file=self.outfile)
+    def gotoWrite(self, label):
+        self._output("goto {}".format(label),file=self.outfile)
 
-        def labelWrite(self, label):
-            print("label {}".format(label),file=self.outfile)
+    def labelWrite(self, label):
+        self._output("label {}".format(label),file=self.outfile)
         
-        def callWrite(self, name, arg_len):
-            print("call {} {}".format(name, arg_len),file=self.outfile)
+    def callWrite(self, name, arg_len):
+        self._output("call {} {}".format(name, arg_len),file=self.outfile)
         
-        def returnWrite(self):
-            print("return",file=self.outfile)
+    def returnWrite(self):
+        self._output("return",file=self.outfile)
         
-        def functionWrite(self, name, local_len):
-            print("function {} {}".format(name,local_len),file=self.outfile)
+    def functionWrite(self, name, local_len):
+        self._output("function {} {}".format(name,local_len),file=self.outfile)
 
-        def expressionWrite(self, term):
-            # ---------------
-            return 0
+    def expressionWrite(self, term):
+        # ---------------
+        return 0
+
+    def _output(self, strtowrite):
+        #Grava a string no arquivo de saída xml. 
+        self.outfile.write(strtowrite)
